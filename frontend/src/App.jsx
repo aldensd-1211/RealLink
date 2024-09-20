9a5448d2a587ff37244bf8a8259f746c9f5b134a
aldensd-1211 <aldendcunha1211@gmail.com>
2024-09-20 10:34:27 +0530

    add routes and controllers

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,36 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AddProperty from "./pages/AddProperty";
 import Home from "./pages/Home";
 import Listing from "./pages/Listing";
 import Bookings from "./pages/Bookings";
 import Favourites from "./pages/Favourites";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Layout from "./components/Layout";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ToastContainer } from "react-toastify";
+import { ReactQueryDevtools } from "react-query/devtools";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
+  const queryClient = new QueryClient();
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/listing" element={<Listing />}></Route>
-        <Route path="/addproperty" element={<AddProperty />}></Route>
-        <Route path="/bookings" element={<Bookings />}></Route>
-        <Route path="/favourites" element={<Favourites />}></Route>
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Suspense fallback={<div>Loading data...</div>}>
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="/listing" element={<Listing />}></Route>
+              <Route path="/addproperty" element={<AddProperty />}></Route>
+              <Route path="/bookings" element={<Bookings />}></Route>
+              <Route path="/favourites" element={<Favourites />}></Route>
+            </Route>
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
+      <ToastContainer />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
--- frontend/src/pages/Listing.jsx
+++ frontend/src/pages/Listing.jsx
@@ -2,8 +2,33 @@ import React from "react";
 import Searchbar from "../components/Searchbar";
 import { PROPERTIES } from "../constant/data";
 import Item from "../components/Item";
+import useProperties from "../hooks/useProperties";
+import { PuffLoader } from "react-spinners";
 
 const Listing = () => {
+  const { data, isError, isLoading } = useProperties();
+  if (isError) {
+    return (
+      <div>
+        <span>Error while fetching data</span>
+      </div>
+    );
+  }
+
+  if (isLoading) {
+    return (
+      <div className="h-64 flexCenter">
+        <PuffLoader
+          height="80"
+          width="80"
+          radius={1}
+          color="#555"
+          aria-label="puff-loading"
+        />
+      </div>
+    );
+  }
+
   return (
     <main className="max-padd-container my-[99px]">
       <div className="max-padd-container py-10 xl:py-22 bg-primary rounded-3xl">
@@ -11,7 +36,7 @@ const Listing = () => {
           <Searchbar />
 
           <div className="grid gap-6 grid-cols-1 md:grid-cols-2 xl:grid-cols-3 mt-10">
-            {PROPERTIES.map((property) => (
+            {data.map((property) => (
               <Item key={property.title} property={property} />
             ))}
           </div>
