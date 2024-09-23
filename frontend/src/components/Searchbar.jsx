ad122fcca83275489598b77c59f95297ea5bbfa6
aldensd-1211 <aldendcunha1211@gmail.com>
2024-09-23 19:09:45 +0530

    Add Search Functionality

--- frontend/src/components/Searchbar.jsx
+++ frontend/src/components/Searchbar.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { FaLocationDot } from "react-icons/fa6";
 
-const Searchbar = () => {
+const Searchbar = ({ filter, setFilter }) => {
   return (
     <div className="flexBetween pl-6 h-[3.3rem] bg-white w-full max-w-[366px] rounded-full ring-1 ring-slate-500/5">
       <input
+        value={filter}
+        onChange={(e) => setFilter(e.target.value)}
         type="text"
         placeholder="Enter residency name/city/country"
         className="bg-transparent border-none outline-none w-full"
--- frontend/src/pages/Listing.jsx
+++ frontend/src/pages/Listing.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Searchbar from "../components/Searchbar";
 import Item from "../components/Item";
 import useProperties from "../hooks/useProperties.jsx";
@@ -6,6 +6,7 @@ import { PuffLoader } from "react-spinners";
 
 const Listing = () => {
   const { data, isError, isLoading } = useProperties();
+  const [filter, setFilter] = useState("");
   if (isError) {
     return (
       <div>
@@ -32,12 +33,22 @@ const Listing = () => {
     <main className="max-padd-container my-[99px]">
       <div className="max-padd-container py-10 xl:py-22 bg-primary rounded-3xl">
         <div>
-          <Searchbar />
+          <Searchbar filter={filter} setFilter={setFilter} />
 
           <div className="grid gap-6 grid-cols-1 md:grid-cols-2 xl:grid-cols-3 mt-10">
-            {data.map((property) => (
+            {data
+              .filter(
+                (property) =>
+                  property.title.toLowerCase().includes(filter.toLowerCase()) ||
+                  property.city.toLowerCase().includes(filter.toLowerCase()) ||
+                  property.country.toLowerCase().includes(filter.toLowerCase())
+              )
+              .map((property) => (
+                <Item key={property.title} property={property} />
+              ))}
+            {/* {data.map((property) => (
               <Item key={property.title} property={property} />
-            ))}
+            ))} */}
           </div>
         </div>
       </div>
